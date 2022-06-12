6a427385437ccde1f6bb48de3e9219d3e74c15b3
vitaly <viv55941@icloud.com>
2022-06-12 12:23:00 +0100

    avoid creating a game with the undefined game id

--- pages/api/joinGame.js
+++ pages/api/joinGame.js
@@ -5,6 +5,10 @@ export default async function handler(req, res) {
   const cookies = new Cookies(req, res);
   const playerId = cookies.get("playerId");
 
+  if (req.body.gameId === '') {
+    return res.redirect('/');
+  }
+
   const gameId = req.body.gameId;
   await addPlayer(gameId, playerId);
   return res.redirect(`/games/${gameId}`);
