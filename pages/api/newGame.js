bda1cf886ebf755651b170fe03596ee2f2142c03
vitaly <viv55941@icloud.com>
2022-06-12 12:21:00 +0100

    avoid creating a game with the zero number of players

--- pages/api/newGame.js
+++ pages/api/newGame.js
@@ -4,6 +4,11 @@ import Cookies from "cookies";
 export default async function handler(req, res) {
   const cookies = new Cookies(req, res);
   const playerId = cookies.get("playerId");
+  const n = req.body.numPlayers;
+
+  if (req.body.numPlayers === '') {
+    return res.redirect('/');
+  }
 
   const gameId = await createNewGame(
     parseInt(req.body.numPlayers, 10),
